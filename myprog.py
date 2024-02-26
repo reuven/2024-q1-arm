9122b5873931ca905065e57df2158d3b90c22231
Reuven Lerner <reuven@lerner.co.il>
2024-02-26 12:25:43 +0200

    gitautosync update 11

---
+++ myprog.py
@@ -0,0 +1,12 @@
+def mysum(numbers):
+    total = 0
+
+    for one_number in numbers:
+        total += one_number
+
+    return total
+
+
+print(mysum([10, 20, 30]))
+print(mysum([10, 20.5, 30]))
+print(mysum([10, 'abcd', 30]))
