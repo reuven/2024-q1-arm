3fa0ffa055bd3e6ef0d7d5ee0f3bd005c9cd574c
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 13:54:45 +0200

    gitautosync update 112

--- executor1.py
+++ executor1.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+from concurrent.futures import ThreadPoolExecutor
+
+
+def square(x):
+    return x ** 2
+
+
+with ThreadPoolExecutor() as executor:
+    for one_number in range(10):
+        one_result = executor.submit(square, one_number)
