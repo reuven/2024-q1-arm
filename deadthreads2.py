76ee8fc49d95438b04340a4246d031bd8792b34e
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 11:58:41 +0200

    gitautosync update 56

---
+++ deadthreads2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import threading
+import time
+
+
+def long_hello_goodbye():
+    print('Hello')
+    time.sleep(5)
+    print('Goodbye')
+
+
+def launch_thread():
+    print('Start launch')
+    t = threading.Thread(target=long_hello_goodbye)
+    t.start()
+    print('End launch')
+
+
+print('Running launch_thread')
+launch_thread()
+print('Done with launch_thread')
