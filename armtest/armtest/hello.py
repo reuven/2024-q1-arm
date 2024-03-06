f1be55ef3a5e1177f502f87d48324465cc46cd22
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 10:42:43 +0200

    gitautosync update 11

--- armtest/armtest/hello.py
+++ armtest/armtest/hello.py
@@ -1,3 +1,5 @@
+import requests
+
 x = 100
 
 y = [10, 20, 30]
@@ -5,3 +7,7 @@ y = [10, 20, 30]
 
 def hello(name):
     return f'Hello, {name}!'
+
+
+def url_len(url):
+    return len(requests.get(url).content)
