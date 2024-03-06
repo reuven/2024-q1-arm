4d706e32a2f18e13aa450b00d5fd74030b82c8e1
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 16:34:48 +0200

    gitautosync update 244

--- test_count_vowels.py
+++ test_count_vowels.py
@@ -2,6 +2,14 @@ import pytest
 from count_vowels import count_vowels
 
 
-@pytest.mark.parameterize()
+@pytest.mark.parametrize('text, counts',
+                         [
+                             ('testing', {'a': 0, 'e': 1,
+                              'i': 1, 'o': 0, 'u': 0}),
+                             ('TESTING', {'a': 0, 'e': 1,
+                              'i': 1, 'o': 0, 'u': 0}),
+                             ('this is yet another test', {
+                              'a': 0, 'e': 1, 'i': 1, 'o': 0, 'u': 0}),
+                         ])
 def test_text(text, counts):
     assert count_vowels(text) == counts
