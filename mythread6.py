dac57113b70fab602dcbe74b2398c85247d403d0
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 12:09:20 +0200

    gitautosync update 75

--- mythread6.py
+++ mythread6.py
@@ -30,5 +30,5 @@ while threading.active_count() > 1:
             else:
                 print(f'\nJoined {one_thread.name}')
 
-while not output.empty:
+while not output.empty():
     print(output.get())
