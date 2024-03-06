bbe2d19cfff57cbe3a6178833330d338326d63ee
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 12:14:34 +0200

    gitautosync update 84

---
+++ mythread8.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import threading
+import time
+import random
+import queue
+
+output = [0]
+
+
+def hello(n):
+    time.sleep(random.randint(0, 3))
+    with lock:
+        # __enter__()
+        print(f'{n} Hello!')
+        output.put(f'Hello from {n}')
+        output.put(f'Goodbye from {n}')
+        print(f'{n} Gkoodbye!')
+        # __exit__()
+
+
+for i in range(10):
+    t = threading.Thread(target=hello, args=(i,), name=f'thread-{i}')
+    t.start()
+
+print('Done!')
+
+while threading.active_count() > 1:
+    for one_thread in threading.enumerate():
+        if one_thread != threading.current_thread():
+            one_thread.join(0.001)   # non-blocking join
+            if one_thread.is_alive():
+                # print(f'\nFailed to join {one_thread.name}; will return to it')
+                pass
+            else:
+                print(f'\nJoined {one_thread.name}')
+
+while not output.empty():
+    print(output.get())
