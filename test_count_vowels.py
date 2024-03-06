ca8d190ee0da1f878f98203c5ac1c5daf90edae8
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 16:36:51 +0200

    gitautosync update 245

--- test_count_vowels.py
+++ test_count_vowels.py
@@ -8,8 +8,8 @@ from count_vowels import count_vowels
                               'i': 1, 'o': 0, 'u': 0}),
                              ('TESTING', {'a': 0, 'e': 1,
                               'i': 1, 'o': 0, 'u': 0}),
-                             ('this is yet another test', {
-                              'a': 0, 'e': 1, 'i': 1, 'o': 0, 'u': 0}),
+                             # ('this is yet another test', {
+                             #  'a': 0, 'e': 1, 'i': 1, 'o': 0, 'u': 0}),
                          ])
 def test_text(text, counts):
     assert count_vowels(text) == counts
