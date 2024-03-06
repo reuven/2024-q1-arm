fb540da90c3f43f2319bc96950bcb1d57440ca87
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 16:42:32 +0200

    gitautosync update 257

--- test_person.py
+++ test_person.py
@@ -1,6 +1,13 @@
 import pytest
 from person import Person
 
+# fixture
+
+
+@pytest.fixture
+def sample_person():
+    return Person('FirstName', 'LastName')
+
 
 def test_repr():
     p = Person('FirstName', 'LastName')
