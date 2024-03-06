62d4a02aaf522142704d4ac816c7b896c52b5911
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 16:41:19 +0200

    gitautosync update 256

--- test_person.py
+++ test_person.py
@@ -1,12 +1,12 @@
 import pytest
 from person import Person
 
-p = Person('FirstName', 'LastName')
-
 
 def test_repr():
+    p = Person('FirstName', 'LastName')
     assert str(p) == 'FirstName LastName'
 
 
 def test_initials():
+    p = Person('FirstName', 'LastName')
     assert p.initials() == 'F L'
