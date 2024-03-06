6adbf25d6142a63fd77022896fbafc4cf90c2004
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 11:41:54 +0200

    gitautosync update 30

---
+++ mythread2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import threading
+import time
+import random
+
+
+def hello(n):
+    time.sleep(random.randint(0, 3))
+    print(f'{n} Hello!')
+
+
+for i in range(10):
+    t = threading.Thread(target=hello, args=(i,))
+    t.start()
