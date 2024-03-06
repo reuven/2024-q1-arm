c91958dfa9a416b3ab8ae5812c3d9f12073835c9
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 14:46:50 +0200

    gitautosync update 165

---
+++ executor3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from concurrent.futures import ProcessPoolExecutor, wait, FIRST_COMPLETED
+import time
+import random
+
+
+def square(x):
+    time.sleep(random.randint(0, 3))
+    return x ** 2
+
+
+with ThreadPoolExecutor(max_workers=10) as executor:
+    all_results = []
+    for one_number in range(20):
+        one_result = executor.submit(square, one_number)
+        all_results.append(one_result)
+
+    done, not_done = wait(
+        all_results, return_when=FIRST_COMPLETED)
+
+    for one_result in done:
+        print(one_result.result())
