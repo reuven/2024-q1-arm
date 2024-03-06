77f14ee932df9114586ff3f2f7993848fb58609d
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 12:35:00 +0200

    gitautosync update 93

--- file_lengths.py
+++ file_lengths.py
@@ -5,11 +5,11 @@ import queue
 
 q = queue.Queue()
 
+
 def file_length(filename):
-    output = 0
+    total_length = 0
 
     for one_line in open(filename):
-        output += len(one_line)
+        total_length += len(one_line)
 
-    q.put()
-    
+    q.put((filename, total_length))
