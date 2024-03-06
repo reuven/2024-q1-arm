d37088fb3eb16771feb091eaf6b71417e9e4fc3e
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 16:06:06 +0200

    gitautosync update 221

---
+++ test_min_and_max_2.py
@@ -0,0 +1,24 @@
+import pytest
+from min_and_max import min_and_max
+
+
+def test_simple():
+    assert min_and_max([10, 20, 30, -5, -7]) == (-7, 30)
+
+
+def test_floats():
+    assert min_and_max([10.5, 20.3, 30.7, -5.2, -7.1]) == (-7.1, 30.7)
+
+
+def test_strings():
+    assert (min_and_max('this is a ridiculous test'.split())) == ('a', 'this')
+
+
+def test_empty():
+    with pytest.raises(ValueError):
+        min_and_max([])
+
+
+# def test_not_empty():
+#     with pytest.raises(ValueError):
+#         min_and_max([10, 20, 30])
