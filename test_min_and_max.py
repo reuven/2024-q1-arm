b6c344f9a0981ce191d567281c03c6c7e12a1f4b
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 16:00:22 +0200

    gitautosync update 208

--- test_min_and_max.py
+++ test_min_and_max.py
@@ -3,3 +3,7 @@ from min_and_max import min_and_max
 
 def test_simple():
     assert min_and_max([10, 20, 30, -5, -7]) == (-7, 30)
+
+
+def test_floats():
+    assert min_and_max([10.5, 20.3, 30.7, -5.2, -7.1]) == (-7.1, 30.7)
