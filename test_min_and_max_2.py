dbb7060f46bada25a7d463bec74d5c395850e8d5
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 16:10:17 +0200

    gitautosync update 225

--- test_min_and_max_2.py
+++ test_min_and_max_2.py
@@ -1,17 +1,30 @@
 import pytest
 from min_and_max import min_and_max
 
-
-def test_simple():
-    assert min_and_max([10, 20, 30, -5, -7]) == (-7, 30)
+# parameterization
 
 
-def test_floats():
-    assert min_and_max([10.5, 20.3, 30.7, -5.2, -7.1]) == (-7.1, 30.7)
+@pytest.parametrize('input_values, expected_tuple',
+                    [                                     # list of tuples to pass to each test
+                        # (input_values, expected_tuple)
+                        ([10, 20, 30, -5, -7], (-7, 30)),
+                        ([10.5, 20.3, 30.7, -5.2, -7.1], (-7.1, 30.7)),
+                        ('this is a ridiculous test'.split(), ('a', 'this'))
+                    ])
+def test_template(input_values, expected_tuple):
+    assert min_and_max(input_values) == expected_tuple
 
 
-def test_strings():
-    assert (min_and_max('this is a ridiculous test'.split())) == ('a', 'this')
+# def test_simple():
+#     assert min_and_max([10, 20, 30, -5, -7]) == (-7, 30)
+
+
+# def test_floats():
+#     assert min_and_max([10.5, 20.3, 30.7, -5.2, -7.1]) == (-7.1, 30.7)
+
+
+# def test_strings():
+#     assert (min_and_max('this is a ridiculous test'.split())) == ('a', 'this')
 
 
 def test_empty():
