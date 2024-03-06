f71a85eb02c523267f307353a706459f25ea9223
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 11:48:00 +0200

    gitautosync update 40

---
+++ mythread4.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import threading
+import time
+import random
+
+
+def hello(n):
+    time.sleep(random.randint(0, 3))
+    print(f'{n} Hello!')
+
+
+all_threads = []
+
+for i in range(10):
+    t = threading.Thread(target=hello, args=(i,), name=f'thread-{i}')
+    all_threads.append(t)
+    t.start()
+
+print('Done!')
+
+for one_thread in all_threads:
+    one_thread.join()
+    print(f'\nJoining {one_thread.name}')
+
+
+print('Starting something new')
