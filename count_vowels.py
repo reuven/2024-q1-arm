b77396682248e3f687cb694c38f7b24ffca6bd62
Reuven Lerner <reuven@lerner.co.il>
2024-03-06 16:31:31 +0200

    gitautosync update 237

--- count_vowels.py
+++ count_vowels.py
@@ -3,4 +3,8 @@
 def count_vowels(text):
     output = dict.fromkeys('aeiou', 0)
 
+    for one_character in text.lower():
+        if one_character in output:
+            output[one_character] += 1
+
     return output
